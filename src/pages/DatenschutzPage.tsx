5cf063ec7b115c3db3936102286f1556c9920240
Gym Karri <gymkarri@gmail.com>
2026-02-03 22:48:28 +0700

    feat: swap datenschutz hero to sea urchin image

--- src/pages/DatenschutzPage.tsx
+++ src/pages/DatenschutzPage.tsx
@@ -11,7 +11,7 @@ const DatenschutzPage = () => {
             {/* Hero Section */}
             <div className="relative h-[40vh] md:h-[50vh] overflow-hidden">
                 <img
-                    src="/images/datenschutz_hero.jpg"
+                    src="/images/datenschutz_sea_urchin.jpg"
                     alt="Datenschutz"
                     className="w-full h-full object-cover"
                 />
